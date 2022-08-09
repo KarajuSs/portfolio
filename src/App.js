0a8d8f1253f40886b9f6bb989f40729a7a025919
KarajuSs <karajussgg@gmail.com>
2022-08-09 03:05:19 +0200

    Utworzenie aplikacji za pomocą kopomentów

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
-import {useEffect} from 'react';
-import styled from 'styled-components'
+import './App.scss'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
 
 export default function App() {
-  useEffect(() => {
-    document.body.classList.add('bg-color');
-    document.body.style.backgroundColor = '#0E1129';
-  }, []);
-
   return (
-    <Wrapper>
-      <box className="box">Test</box>
-    </Wrapper>
+    <>
+    <Routes>
+      <Route path="/" element={<Layout />}/>
+    </Routes>
+    </>
   );
 };
-
-/*const Button = styled.button`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid palevioletred;
-  color: palevioletred;
-  margin: 0 1em;
-  padding: 0.25em 1em;
-`*/
-
-const Wrapper = styled.div`
-  color: white;
-  margin: 0 auto;
-  }
-`
