d48ad673092988582eb29f9697c0a5f861045d95
KarajuSs <karajussgg@gmail.com>
2022-08-09 21:52:53 +0200

    Skończono navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,41 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
 import Logo from '../../assets/images/logo-K_32x32.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAddressBook, faBook, faHome } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => (
     <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            <img src={Logo} alt='logo'/>
-            <span className='subtitle'>Kamil Lewicki</span>
-        </Link>
+        <div id='container'>
+            <div className='nav-table'>
+                <Link className='logo d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none' to='/'>
+                    <img src={Logo} alt='logo'/>
+                    <span className='subtitle'>Kamil Lewicki</span>
+                </Link>
+                <nav>
+                    <ul>
+                        <li>
+                            <NavLink exact="true" activeclassname="active" to="/">
+                                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+                                <span className="text">Home</span>
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                                <FontAwesomeIcon icon={faAddressBook} color="#4d4d4e"/>
+                                <span className="text">About</span>
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact="true" activeclassname="active" className="projects-link" to="/projects">
+                                <FontAwesomeIcon icon={faBook} color="#4d4d4e"/>
+                                <span className="text">Projects</span>
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
     </div>
 )
 
