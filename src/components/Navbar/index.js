5c345ab5acdfef5f228518841ffd31b76d45f6a0
KarajuSs <karajussgg@gmail.com>
2022-08-20 20:58:53 +0200

    Zmieniono konstrukcję navbara

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,36 +6,21 @@ import { faAddressBook, faBook, faHome } from '@fortawesome/free-solid-svg-icons
 
 const Navbar = () => (
     <div className='nav-bar'>
-        <div id='container'>
-            <div className='nav-table'>
-                <Link className='logo d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none' to='/'>
-                    <img src={Logo} alt='logo'/>
-                    <span className='subtitle'>Kamil Lewicki</span>
-                </Link>
-                <nav className='d-none d-lg-table-cell'>
-                    <ul>
-                        <li>
-                            <NavLink exact="true" activeclassname="active" to="/">
-                                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
-                                <span className="text">Główna</span>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-                                <FontAwesomeIcon icon={faAddressBook} color="#4d4d4e"/>
-                                <span className="text">O mnie</span>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink exact="true" activeclassname="active" className="projects-link" to="/projects">
-                                <FontAwesomeIcon icon={faBook} color="#4d4d4e"/>
-                                <span className="text">Projekty</span>
-                            </NavLink>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
+        <Link className='logo' to='/'>
+            <img src={Logo} alt='logo'/>
+            <span className='subtitle'>Kamil Lewicki</span>
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faAddressBook} color="#4d4d4e"/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="projects-link" to="/projects">
+                <FontAwesomeIcon icon={faBook} color="#4d4d4e"/>
+            </NavLink>
+        </nav>
     </div>
 )
 
